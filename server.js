bde33ef6cd066af760d8a807e285eee90a1b3cfa
Juan Tarrel <juantarrel@gmail.com>
2020-06-02 11:29:20 -0700

    cjange

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world! >>>5<<<\n');
+  res.send('Hello world! >>>6<<<\n');
 });
 
 app.get('/build-info', (req, res) => {
